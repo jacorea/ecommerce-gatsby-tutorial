eea327c23cd5a4d0cda7a299ce874b663a1b3d5b
jcorea <jacorea410@gmail.com>
2019-06-26 11:31:51 -0700

    code for hitting strip API, and added firstname and lastname fields for donation form.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+export default class IndexPage extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  handleInputChange = event => { 
+      const target = event.target 
+      const value = target.value 
+      const name = target.name
 
-export default IndexPage
+      this.setState({[name]: value,})
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          First name
+          <input 
+            type="text" 
+            name="firstName" 
+            onChange={this.handleInputChange} 
+            value={this.state.firstName}/>
+        </label>
+        <label>
+          Last name
+          <input 
+            type="text" 
+            name="lastName"
+            onChange={this.handleInputChange} 
+            value={this.state.firstName} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
