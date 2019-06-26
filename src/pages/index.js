e7318b83b64aec2538cc444c8cc4da13de81e29f
jcorea <jacorea410@gmail.com>
2019-06-26 12:33:32 -0700

    add basic signup fields for a donation form.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,35 +6,69 @@ export default class IndexPage extends React.Component {
     lastName: "",
   }
 
-  handleInputChange = event => { 
-      const target = event.target 
-      const value = target.value 
-      const name = target.name
+//   handleInputChange = (e) => { 
+//       console.log(e.target.value)
+     
 
-      this.setState({[name]: value,})
-  }
+//       this.setState({[e.target.name]: e.target.value,})
+//   }
+
+//   handleSubmit = e => {
+//       const {firstName, lastName } = this.state
+//       e.preventDefault()
+//       alert(`welcome ${firstName} ${lastName}!`)
+//   }
+
+
 
   render() {
     return (
-      <form>
-        <label>
-          First name
-          <input 
-            type="text" 
-            name="firstName" 
-            onChange={this.handleInputChange} 
-            value={this.state.firstName}/>
-        </label>
-        <label>
-          Last name
-          <input 
-            type="text" 
-            name="lastName"
-            onChange={this.handleInputChange} 
-            value={this.state.firstName} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+        <form>
+            <div className="form-row">
+                <div className="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" className="form-control" id="inputEmail4" placeholder="Email"/>
+                </div>
+                <div className="form-group col-md-6">
+                <label for="inputPassword4">Password</label>
+                <input type="password" className="form-control" id="inputPassword4" placeholder="Password"/>
+                </div>
+            </div>
+            <div className="form-group">
+                <label for="inputAddress">Address</label>
+                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+            </div>
+            <div className="form-group">
+                <label for="inputAddress2">Address 2</label>
+                <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+            </div>
+            <div className="form-row">
+                <div className="form-group col-md-6">
+                <label for="inputCity">City</label>
+                <input type="text" className="form-control" id="inputCity"/>
+                </div>
+                <div className="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" className="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+                </div>
+                <div className="form-group col-md-2">
+                <label for="inputZip">Zip</label>
+                <input type="text" className="form-control" id="inputZip"/>
+                </div>
+            </div>
+            <div className="form-group">
+                <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck"/>
+                <label className="form-check-label" for="gridCheck">
+                    Check me out
+                </label>
+                </div>
+            </div>
+            <button type="submit" className="btn btn-primary">Sign in</button>
+        </form>
     )
   }
 }
