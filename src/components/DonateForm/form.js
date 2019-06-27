2f56378de40ba063fc42612a03945c6b24368a30
jcorea <jacorea410@gmail.com>
2019-06-27 12:03:02 -0700

    Disabled submit button on donation form until required fields are filled. Validation errors for required fields once touched show up.

--- src/components/DonateForm/form.js
+++ src/components/DonateForm/form.js
@@ -5,16 +5,107 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
 export const Form = (props) => {
+  const {
+    values: { firstName, lastName, address1, address2, city, state, zip },
+    errors,
+    touched, 
+    handleChange,
+    isValid,
+    setFieldTouched,
+  } = props;
+
+  const change = (name,e) => {
+    e.persist();
+    handleChange(e);
+    setFieldTouched(name, true, false);
+    console.log(name)
+  }
   return (
-   <form onSubmit={() => {}}>
-     <TextField id="firstName" name="firstName" label="First Name" fullWidth type="text" />
-     <TextField id="lastName"name="lastName"label="Last Name"fullWidth type="text" />
-     <TextField id="address1" name="address1" label="Address" fullWidth type="text"/>
-     <TextField id="address2" name="address2" label="Apt/Suite" fullWidth type="text"/>
-     <TextField id="city" name="city" label="City" fullWidth type="text"/>
-     <TextField id="state" name="state" label="State" fullWidth type="text"/>
-     <TextField id="zip" name="zip" label="Zip" fullWidth />
-     <Button type="submit" variant="raised" color="primary" fullWidth> Submit </Button>
+   <form onSubmit={() => {
+     alert("submitted")}
+    }>
+     <TextField 
+      id="firstName" 
+      name="firstName" 
+      label="First Name"
+      helperText={touched.firstName ? errors.firstName : ""}
+      error={touched.firstName && Boolean(errors.firstName)}
+      value={firstName}
+      onChange={change.bind(null, "firstName")} 
+      fullWidth
+     />
+     <TextField 
+        id="lastName" 
+        name="lastName" 
+        label="Last Name"
+        helperText={touched.lastName ? errors.lastName : ""}
+        error={touched.lastName && Boolean(errors.lastName)}
+        value={lastName}
+        onChange={change.bind(null, "lastName")}  
+        fullWidth 
+        type="text" 
+      />
+     <TextField 
+        id="address1" 
+        name="address1" 
+        label="Address"
+        helperText={touched.address1 ? errors.address1 : ""}
+        error={touched.address1 && Boolean(errors.address1)}
+        value={address1}
+        onChange={change.bind(null, "address1")}  
+        fullWidth 
+        type="text"
+      />
+     <TextField 
+        id="address2" 
+        name="address2" 
+        label="Apt/Suite"
+        value={address2}
+        onChange={change.bind(null, "address2")} 
+        fullWidth 
+        type="text"
+      />
+     <TextField 
+        id="city" 
+        name="city" 
+        label="City"
+        helperText={touched.city ? errors.city : ""}
+        error={touched.city && Boolean(errors.city)}
+        value={city}
+        onChange={change.bind(null, "city")} 
+ 
+        fullWidth 
+        type="text"
+      />
+     <TextField city
+         id="state"
+        name="state" 
+        label="State"
+        helperText={touched.state ? errors.state : ""}
+        error={touched.state && Boolean(errors.state)}
+        value={state}
+        onChange={change.bind(null, "state")}  
+        fullWidth 
+        type="text"
+      />
+     <TextField 
+        id="zip" 
+        name="zip" 
+        label="Zip"
+        helperText={touched.zip ? errors.zip : ""}
+        error={touched.zip && Boolean(errors.zip)}
+        value={zip}
+        onChange={change.bind(null, "zip")}
+        fullWidth 
+      />
+     <Button 
+        type="submit" 
+        variant="raised" 
+        color="primary"
+        disabled={!isValid} 
+        fullWidth
+      > Submit
+      </Button>
    </form>
  );
 };
