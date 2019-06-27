59249fc23bc5d6a6b80589b086f1875adf57c4c8
jcorea <jacorea410@gmail.com>
2019-06-27 11:08:00 -0700

    Add Yup API for formik form validation. Wrote validation for basic signup form with required validation for all fields except address2.

--- src/components/DonateForm/index.js
+++ src/components/DonateForm/index.js
@@ -3,6 +3,7 @@ import { Formik } from "formik";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Form } from "./form";
 import Paper from "@material-ui/core/Paper";
+import * as Yup from "yup";
 
 const styles = theme => ({
  paper: {
@@ -18,6 +19,15 @@ const styles = theme => ({
  }
 });
 
+const validationSchema= Yup.object({
+  firstName: Yup.string("Please enter your First Name").required("First Name is required"),
+  lastName: Yup.string("Please enter your Last Name").required("Last Name is required"),
+  address1: Yup.string("Please enter your Address").required("Address is required"),
+  city: Yup.string("Please enter your City").required("City location is required"),
+  state: Yup.string("Please enter your State").required("State is required"),
+  zip: Yup.string("Please enter your Zip Code").required("Zip Code is required"),
+})
+
 class DonateForm extends Component {
   constructor(props) {
     super(props);
@@ -26,6 +36,7 @@ class DonateForm extends Component {
  
   render() {
    const {classes} = this.props;
+   const values = {firstName:"", lastName:"", address1:"", address2:"", city:"", state:"", zip:""}
    return (
    <React.Fragment>
      <div className={classes.container}>
@@ -33,6 +44,8 @@ class DonateForm extends Component {
          <h1> Donation Form</h1>
          <Formik
              render={props => <Form {...props} />}
+             initialValues={values}
+             validationSchema={validationSchema}
          />
          </Paper>
      </div>
